152825e662bf85aedebccc85414ce7a1a762658b
phthallo <annabelquach13@gmail.com>
2024-09-25 12:38:55 +0930

    small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://phthallo.com/',
   //output: "server",
-  integrations: [tailwind({applyBaseStyles:false})],
+  integrations: [
+    tailwind({applyBaseStyles:false}),
+    sitemap({
+			filter: page => page !== 'https://phthallo.com/404'
+		})]
 });

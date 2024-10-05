13d3b6f180e81ebb9816c6737b8a2955e4854f20
phthallo <annabelquach13@gmail.com>
2024-10-05 16:19:06 +0930

    feat: z-coord support for bezier curve

--- public/scripts/Bezier.js
+++ public/scripts/Bezier.js
@@ -9,12 +9,14 @@ function store(){
     calculate(values);
 }
 function calculate(vals){
-    let [[x0, y0], [x1, y1], [x2, y2], [x3, y3]] = vals;
+    let [[x0, y0, z0], [x1, y1], [x2, y2], [x3, y3, z3]] = vals;
     const result = document.querySelector(".result");
     result.innerHTML = `
     x(t) = ${+(x3-3*x2+3*x1-x0).toFixed(3)}t<sup>3</sup> + ${+(3*x2-6*x1+3*x0).toFixed(3)}t<sup>2</sup> + ${+(3*x1-3*x0).toFixed(3)}t + ${x0}
     <br>
     y(t) = ${+(y3-3*y2+3*y1-y0).toFixed(3)}t<sup>3</sup> + ${+(3*y2-6*y1+3*y0).toFixed(3)}t<sup>2</sup> + ${+(3*y1-3*y0).toFixed(3)}t + ${y0}
+    <br>
+    z(t) = ${z3-z0}t + ${z0}
     `
 }
 

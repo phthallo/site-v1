d61438d8d2ab672b656439e72cc4c1e34e381121
phthallo <annabelquach13@gmail.com>
2024-10-07 17:56:05 +1030

    feat: blog, tags feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://phthallo.com/',
   //output: "server",
-  integrations: [
-    tailwind({applyBaseStyles:true}),
-    sitemap({
-			filter: page => page !== 'https://phthallo.com/404'
-		})]
+  integrations: [tailwind({applyBaseStyles:true}), sitemap({
+          filter: page => page !== 'https://phthallo.com/404'
+      }), mdx()]
 });

b09c5e1acdfb00ec0745413c6fdb3b6447ea11d1
aqua <84078890+phthallo@users.noreply.github.com>
2025-06-15 17:48:14 -0400

    fix: switch to hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
-
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  
   site: 'https://phthallo.com/',
-  //output: "server",
+  output: "hybrid",
+  adapter: node({mode: "standalone"}),
   integrations: [tailwind({applyBaseStyles:true}), sitemap({
           filter: page => page !== 'https://phthallo.com/404'
       }), mdx()]

8f606507eed0649237a6c00b5bbd5a2340de5a2b
aqua <84078890+phthallo@users.noreply.github.com>
2025-10-26 21:27:58 -0400

    fix: content collection for rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,12 @@
 import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   return rss({
     title: 'phthallo',
     description: 'musings and rambles of a sleep-deprived teen',
     site: context.site,
-    items: await pagesGlobToRssItems(
-        import.meta.glob("./blog/*.mdx")
-    ),
+    items: await getCollection("blog"),
     customData: `<language>en-au</language>`,
   });
 }

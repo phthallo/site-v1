757c9bf5b3689ec02f4edb55f742ef66dff51ca9
aqua <84078890+phthallo@users.noreply.github.com>
2025-10-26 21:45:32 -0400

    fix: og images, rss caused by shift to content collections

--- src/pages/open-graph/[...route].ts
+++ src/pages/open-graph/[...route].ts
@@ -1,12 +1,14 @@
 import { OGImageRoute } from 'astro-og-canvas';
 import { getCollection } from 'astro:content';
 
+export const prerender = true
+
 export const { getStaticPaths, GET } = OGImageRoute({
   param: 'route',
   pages: await getCollection("blog"),
   getImageOptions: (path, page) => ({
-    title: page.frontmatter.title,
-    description: page.frontmatter.description,
+    title: page.data.title,
+    description: page.data.description,
     logo: { path: 'public/favicon.ico' },
     bgGradient: [[19, 15, 25]],
     font: { title: { families: [ "0xProto", "monospace" ] }, description: { families: [ "0xProto", "monospace" ] } }, 
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,17 @@ import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog')
   return rss({
     title: 'phthallo',
     description: 'musings and rambles of a sleep-deprived teen',
     site: context.site,
-    items: await pagesGlobToRssItems(
-        await getCollection("blog")
-    ),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.id}/`,
+    })),
     customData: `<language>en-au</language>`,
   });
 }

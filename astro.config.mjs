b867d2519500f586ec1d5c706ca38c5f76b732a0
aqua <84078890+phthallo@users.noreply.github.com>
2025-12-11 16:07:14 -0500

    add search bar; update light mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import node from '@astrojs/node';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  
   site: 'https://phthallo.com/',
-  output: "server",
-  adapter: node({mode: "standalone"}),
+  output: "static",
   integrations: [tailwind({applyBaseStyles:true}), sitemap({
           filter: page => page !== 'https://phthallo.com/404'
-      }), mdx()]
+      }), mdx(), pagefind()]
 });

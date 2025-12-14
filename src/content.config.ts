f602f6594bc3b4ffe0c8614573e7e971af5e54e2
aqua <84078890+phthallo@users.noreply.github.com>
2025-12-13 22:00:37 -0500

    adjust image scaling

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.mdx', base: "./src/posts" }),
+    loader: glob({ pattern: '*/[^_]*.mdx', base: "./src/posts" }),
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
